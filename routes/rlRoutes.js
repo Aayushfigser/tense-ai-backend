6282105f030b612215ed1cbe71ef85b5c555fd3d
Ayush maurya <166299050+Aayushfigser@users.noreply.github.com>
2024-10-05 11:57:41 +0530

    Create rlRoutes.js

---
+++ routes/rlRoutes.js
@@ -0,0 +1,11 @@
+//rlRoutes.js
+const express = require('express');
+const { updateRL } = require('../controllers/rlController');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Route to handle updates from the frontend
+router.post('/update', protect, updateRL);
+
+module.exports = router;
